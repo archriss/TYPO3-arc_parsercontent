58e7091d97068229fa92fbdcf9f4148522cb27fd
Christophe Monard <cmonard@archriss.com>
2022-02-16 11:27:06 +0100

    [TASK] composer : make extension compatible 9.5 || 10.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Archriss',
     'state' => 'stable',
     'clearCacheOnLoad' => 0,
-    'version' => '10.4',
+    'version' => '10.4.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
